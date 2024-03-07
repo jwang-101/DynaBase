159de07ab22f9c6e015b17d3d8f29b04955f8db7
Ben Hutz <bn4941@gmail.com>
2024-03-07 14:02:53 -0600

    move connection information to database.ini

--- connect.py
+++ connect.py
@@ -3,21 +3,18 @@
 
 import psycopg2
 import psycopg2.extras
+
+from config import load_config
+
+config = load_config()
 try:
-    my_session = psycopg2.connect(database="dad",
-                        host="localhost",
-                        user="dad_user",
-                        password="dad_pass",
-                        port="5432")
+    # connecting to the PostgreSQL server
+    my_session = psycopg2.connect(**config)
+    print('Connected to the PostgreSQL server.')
 
-#    my_session = psycopg2.connect(database="dad",
-#                        host="ep-jolly-field-a5mt8r6k.us-east-2.aws.neon.tech",
-#                        user="web-app",
-#                        password="",
-#                        port="5432")
+except (psycopg2.DatabaseError, Exception) as error:
+    print(error)
 
-except:
-    print("Unable to connect to the database")
 
 my_cursor = my_session.cursor(cursor_factory=psycopg2.extras.DictCursor)
 

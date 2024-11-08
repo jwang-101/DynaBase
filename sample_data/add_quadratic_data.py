2fc86e78154a568d12b8c6fac8b6ccbccfbb9141
jwang-101 <justin.wang@slu.edu>
2024-11-08 04:43:50 -0600

    small changes to get logs to show up in terminal

--- sample_data/add_quadratic_data.py
+++ sample_data/add_quadratic_data.py
@@ -1,4 +1,3 @@
-
 from functions.function_dim_1_helpers_NF import model_in_database_NF
 
 from functions.function_dim_1_helpers_NF import add_function_all_NF
@@ -6,6 +5,8 @@ from functions.function_dim_1_helpers_NF import add_function_all_NF
 from sage.parallel.ncpus import ncpus
 
 from sage.parallel.use_fork import p_iter_fork
+
+import warnings
 
 ###################################
 ###connect to database
@@ -21,21 +22,32 @@ load("connect.py")
 path_to_log = "/mnt/c/Users/glisc/projects/DynaBase/sample_data/quadratic_log.txt"
 log_file = open(path_to_log, 'w', 1)
 
+warnings.filterwarnings("ignore")
+
 #x^2 + c
 P=ProjectiveSpace(QQ,1,'x,y')
 x,y = P.gens()
 
 def parallel_function(F):
+    load("connect.py")
     if not model_in_database_NF(F, my_cursor)[0]:
-        return add_function_all_NF(F, my_cursor, citations=['Poonen1998'], log_file=log_file)
+        result =  add_function_all_NF(F, my_cursor, citations=['Poonen1998'], log_file=log_file)
+    else:
+        result = 0
+    my_session.commit()
+    my_session.close()
+    return result
 
 # Calling possible_periods for each prime in parallel
 parallel_data = []
-for c in QQ.range_by_height(8): #test bigger numbers here; crash error or timeout error #check if parallelism works without crashing
+P = ProjectiveSpace(QQ, 1, 'x,y')
+x, y = P.gens()
+
+for c in QQ.range_by_height(9): #test bigger numbers here; crash error or timeout error #check if parallelism works without crashing
     F=DynamicalSystem([x**2+c*y**2,y**2]) #polys
     parallel_data.append(((F,), {}))
 
-parallel_iter = p_iter_fork(ncpus()-1, 0) 
+parallel_iter = p_iter_fork(max(1, ncpus()-1), 0) 
 parallel_results = list(parallel_iter(parallel_function, parallel_data))
 
 my_session.commit()
--- sample_data/my_samples.sage.py
+++ sample_data/my_samples.sage.py
@@ -7,10 +7,6 @@ load_attach_path('/mnt/c/Users/glisc/projects/DynaBase')
 import sys
 sys.path.append("/mnt/c/Users/glisc/projects/DynaBase")
 
-# set up tables
-load("connect.py")
-load("setup_tables/setup_all.py")
-
 # quadratic data
 load("connect.py")
 load("sample_data/add_quadratic_data.py")
